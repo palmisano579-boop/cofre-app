a77f5c233007d45543d1e9018926a4acf0cebb0e
palmisano579-boop <palmisano579@gmail.com>
2026-07-09 21:20:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'cofre-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
